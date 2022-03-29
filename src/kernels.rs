e3df127dd77285cd672e95ded2910cdb21b0d2e4
Joey <josephharrison1@me.com>
2022-03-29 17:43:53 +0100

    rayon'd

--- src/kernels.rs
+++ src/kernels.rs
@@ -1,47 +1,43 @@
+use rayon::prelude::*;
+
 pub fn ddot(x: &[f32], y: &[f32]) -> f32 {
-    let mut sum = 0.0;
-    for i in 0..x.len() {
-        sum += x[i] * y[i];
-    }
-    sum
+    x.par_iter().zip(y.par_iter()).map(|(x, y)| x * y).sum()
 }
 
 pub fn ddot_same(x: &[f32]) -> f32 {
-    let mut sum = 0.0;
-    for i in x {
-        sum += i * i
-    }
-    sum
+    x.par_iter().map(|x| x * x).sum()
 }
 
 pub fn wxmy(x: &[f32], y: &[f32], w: &mut [f32]) {
-    for i in 0..w.len() {
-        w[i] = x[i] - y[i];
-    }
+    w.par_iter_mut()
+        .zip(x.par_iter().zip(y.par_iter()))
+        .for_each(|(w, (x, y))| *w = x - y);
 }
 
 pub fn xxpby(x: &mut [f32], b: f32, y: &[f32]) {
-    for i in 0..x.len() {
-        x[i] += b * y[i];
-    }
+    x.par_iter_mut()
+        .zip(y.par_iter())
+        .for_each(|(x, y)| *x += b * y);
 }
 
 pub fn yxpby(x: &[f32], b: f32, y: &mut [f32]) {
-    for i in 0..x.len() {
-        y[i] = x[i] + b * y[i];
-    }
+    y.par_iter_mut()
+        .zip(x.par_iter())
+        .for_each(|(y, x)| *y = x + b * *y);
 }
 
 pub fn sparsemv(a: &crate::Mesh, x: &[f32], y: &mut [f32]) {
-    for i in 0..a.nrow {
-        let mut sum = 0.0;
-        let nnz = a.nnz[i];
-        let values = &a.values[i];
-        let idx = &a.idx[i];
-
-        for j in 0..nnz {
-            sum += values[j] * x[idx[j]];
-        }
-        y[i] = sum;
-    }
+    y.par_iter_mut()
+        .zip(
+            a.nnz
+                .par_iter()
+                .zip(a.values.par_iter().zip(a.idx.par_iter())),
+        )
+        .for_each(|(y, (nnz, (values, idx)))| {
+            let mut sum = 0.0;
+            for j in 0..*nnz {
+                sum += values[j] * x[idx[j]]
+            }
+            *y = sum
+        });
 }
